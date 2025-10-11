de0dbe15309f81e7855150542a8da50ae2729d93
LEOMAR GILBERTO DA SILVA JUNIOR <05326843030@senacrs.edu.br>
2025-10-10 21:53:43 -0300

    adicionado a pasta aula11

---
+++ Aulas/Aula11/script.js
@@ -0,0 +1,41 @@
+//Look maluco
+
+/*let digiteNumero1 = Number(prompt("Me diga quantos acessórios você está usando:"))
+let digiteNumero2 = Number(prompt("Me diga quantas cores você está usando"))
+let digiteNumero3 = Number(prompt("Me diga o fator brilho que você está usando (de 1 a 10)"))
+
+let numerosPontuacao = (digiteNumero1  *5) + (digiteNumero2 *3) + (digiteNumero3 *10)
+console.log(numerosPontuacao)
+
+metaPontuacao = 80
+
+aprovado = numerosPontuacao >= metaPontuacao
+reprovado = numerosPontuacao < metaPontuacao
+
+if (numerosPontuacao >= metaPontuacao) {
+    console.log("Aprovado")
+}
+else 
+{
+    console.log("Reprovado")
+}
+*/
+
+let descricao = prompt("Descreva seu look de hoje:")
+let corPrincipal = prompt("Qual a cor principal do seu look?")
+
+console.log("Descrição do look:", descricao.length)
+
+let temTamanhoAdequado = descricao.length >= 20
+console.log("O look tem  mais de 20 letras?", temTamanhoAdequado)
+
+descricao = descricao.toLowerCase()
+console.log("Descrição do look:", descricao)
+
+let = ["Preto", "Branco", "Rosa", "Cinza", "Laranja", "Amarelo", "Vermelho", "Azul"]
+
+
+
+
+
+
